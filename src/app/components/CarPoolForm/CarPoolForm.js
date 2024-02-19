8e5e927d2c2a50dfade68ba64d0e808dc5a41fb5
Nacho <nachofernandez.1995@hotmail.com>
2024-02-18 23:39:43 -0300

    Refactor CarPoolForm.js to calculate individual savings

--- src/app/components/CarPoolForm/CarPoolForm.js
+++ src/app/components/CarPoolForm/CarPoolForm.js
@@ -17,28 +17,36 @@ export default function CarPoolForm() {
     const [baseFare, setBaseFare] = useState('');
     const [serviceFee, setServiceFee] = useState('');
     const [highDemandFee, setHighDemandFee] = useState('');
+    const [splits, setSplits] = useState([]);
 
-    function calculateSplit(baseFare, serviceFee, highDemandFee, distances) {
-        const totalFare = baseFare + serviceFee + highDemandFee;
+
+    function calculateSplit(baseFare, serviceFee, highDemandFee, distances, individualDistances) {
+
         const totalDistance = distances.reduce((a, b) => a + b, 0);
-        const feeSplit = (serviceFee + highDemandFee) / distances.length;
+        const totalFare = Number(baseFare) + Number(serviceFee) + Number(highDemandFee);
+        const feeSplit = (Number(serviceFee) + Number(highDemandFee)) / distances.length;
 
-        const splits = distances.map(distance => {
-            const distanceSplit = (baseFare * distance) / totalDistance;
-            return distanceSplit + feeSplit;
-        });
+        const priceByKm = baseFare / totalDistance;
 
-        const sumOfSplits = splits.reduce((a, b) => a + b, 0);
+        const individualCosts = individualDistances.map(distance => distance * priceByKm);
+        const individualSum = individualCosts.reduce((a, b) => a + b, 0);
+        const totalSavings = individualSum - baseFare;
+
+        const individualSavingsPercentage = individualCosts.map(cost => cost / individualSum);
+        const individualSavingsAmount = individualSavingsPercentage.map(percentage => percentage * totalSavings);
+
+        const split = individualCosts.map((cost, index) => cost - individualSavingsAmount[index] + feeSplit);
+        const sumOfSplits = split.reduce((a, b) => a + b, 0);
 
         if (Math.abs(totalFare - sumOfSplits) > 0.01) {
+
             console.error('The sum of the splits does not equal the total fare');
         }
 
-        return splits;
+        return split;
     }
 
-    const [splits, setSplits] = useState([]);
-    async function getDistances(addresses) {
+    async function getCarPoolDistances(addresses) {
         const distances = [];
         for (let i = 0; i < addresses.length - 1; i++) {
             // Convert the addresses to coordinates
@@ -67,11 +75,42 @@ export default function CarPoolForm() {
         return distances;
     }
 
+    async function getCarPoolIndividualDistances(addresses) {
+        const distances = [];
+        for (let i = 1; i <= addresses.length - 1; i++) {
+            // Convert the addresses to coordinates, origin will always be the first one
+            const originResponse = await fetch(`http://localhost:3000/api/googlemaps/geocode?address=${encodeURIComponent(addresses[0])}`);
+            const destinationResponse = await fetch(`http://localhost:3000/api/googlemaps/geocode?address=${encodeURIComponent(addresses[i])}`);
+
+            const originData = await originResponse.json();
+            const destinationData = await destinationResponse.json();
+
+            // Check if the API responses contain results
+            if (!originData.results[0] || !destinationData.results[0]) {
+                console.error('Invalid address:', addresses[0], addresses[i]);
+                continue;
+            }
+
+            // Get the coordinates from the Geocoding API responses
+            const origin = originData.results[0].geometry.location;
+            const destination = destinationData.results[0].geometry.location;
+
+            // Calculate the distance between the coordinates
+            const distanceResponse = await fetch(`http://localhost:3000/api/googlemaps/distancematrix?origins=${origin.lat},${origin.lng}&destinations=${destination.lat},${destination.lng}`);
+            const distanceData = await distanceResponse.json();
+            const distance = distanceData.rows[0].elements[0].distance.value;
+
+            distances.push(distance);
+        }
+        return distances;
+    }
+
     const handleSubmit = async (event) => {
         setLoading(true);
         event.preventDefault();
-        const distances = await getDistances(addresses);
-        const calculatedSplits = calculateSplit(baseFare, serviceFee, highDemandFee, distances);
+        const distances = await getCarPoolDistances(addresses);
+        const individualDistances = await getCarPoolIndividualDistances(addresses);
+        const calculatedSplits = calculateSplit(baseFare, serviceFee, highDemandFee, distances.map(d => d / 1000), individualDistances.map(d => d / 1000));
         setSplits(calculatedSplits);
         setLoading(false);
     }
@@ -128,12 +167,12 @@ export default function CarPoolForm() {
                         />
                     </div>
                 </div>
-                {loading ? <p>Calculating splits...</p> :
+                {loading ? <p className="block text-sm font-medium text-gray-700">Calculating splits...</p> :
                     <button type="submit" onClick={handleSubmit} className="w-full py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 active:bg-indigo-700 transition duration-150 ease-in-out">Calculate Splits</button>
                 }
                 {splits.map((split, index) => (
                     <div key={index}>
-                        <p>Stop {index + 1}: {split.toFixed(2)}</p>
+                        <p className="block text-sm font-medium text-gray-700">Stop {index + 1}: {split.toFixed(2)}</p>
                     </div>
                 ))}
             </div>
